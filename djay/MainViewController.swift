bd896e16681a756c8b8d1780872be7481a1976bd
Marco Mussini <marco.mussini@shapr3d.com>
2025-04-30 23:07:58 +0200

    Show app title label only after dismissing OnboardingPagesViewController

--- djay/MainViewController.swift
+++ djay/MainViewController.swift
@@ -3,27 +3,37 @@ import UIKit
 final class MainViewController: UIViewController {
     var hasSeenOnboarding = false
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
+    private let appTitleLabel: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
         label.font = UIFont.preferredFont(forTextStyle: .largeTitle)
         label.text = "djay"
         label.adjustsFontForContentSizeCategory = true
         label.textColor = UIColor(red: 1.0, green: 0.8, blue: 0.0, alpha: 1.0)
-        view.addSubview(label)
+        label.isHidden = true
+
+        return label
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+        view.addSubview(appTitleLabel)
 
         NSLayoutConstraint.activate([
-            label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            label.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            appTitleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            appTitleLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor),
         ])
     }
 
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
 
-        guard !hasSeenOnboarding else { return }
+        guard !hasSeenOnboarding else {
+            appTitleLabel.isHidden = false
+            return
+        }
 
         defer {
             hasSeenOnboarding = true
